ae4be453feb29f629702898ff5a9b977875cadad
nola360 <adaramolajr@gmail.com>
2019-12-22 17:08:30 -0500

    addeadded presentation folder and video

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const keys = document.querySelectorAll('.key');
 const white_keys = document.querySelectorAll('.key.white');
 const black_keys = document.querySelectorAll('.key.black');
 
+
 // Event listener to allow key to trigger playNote function
 keys.forEach(key => key.addEventListener('click', () => playNotes(key)))
 
+
+// KeyBoard Keys Array
 const whiteKeys = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 const blackKeys = ['s', 'd', 'g', 'h', 'j'];
 
@@ -17,12 +20,14 @@ document.addEventListener('keydown', e => {
   const key = e.key;
   const whiteKeyIndex = whiteKeys.indexOf(key);
   const blackKeyIndex = blackKeys.indexOf(key);
+
   // Plays key when pressed
   if (whiteKeyIndex > -1) playNotes(white_keys[whiteKeyIndex]);
   if (blackKeyIndex > -1) playNotes(black_keys[blackKeyIndex]);
 
-  // Adds color affect to key when pressed
+  // // Adds color affect to key when pressed
   key.classList.add('active');
+
   // Removes color effect when key is no longer pressed
   noteAudio.addEventListener('ended', () => {
     key.classList.remove('active');
